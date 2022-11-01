434d3e99cc7de999545196ce1efda1f84ee62e4b
URK21CS2063 <100009596+URK21CS2063@users.noreply.github.com>
2022-11-01 21:04:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,43 +1,65 @@
-class BinaryTreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.left_child = None
-        self.right_child = None
+from typing import List
 
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1 = x + y
+  nums1 = merge_sort(nums1)
+  return nums1
+  
+  
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
 
-def insert(root, new_value) -> BinaryTreeNode:
-    """If binary search tree is empty, make a new node, declare it as root and return the root.
-        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
-        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
-        Finally, return the root.
-        """
-    # Write your code here
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
 
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k=0
+        
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
 
-def inorder(root) -> None:
-    # Write your code here
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
 
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+  return myList
 
-def preorder(root) -> None:
-    # Write your code here
-
-
-def postorder(root) -> None:
-    # Write your code here
 
 
 # Do not change the following code
-input_data = input()
-flag = True
-root = None
-for item in input_data.split(', '):
-    if flag is True:
-        root = insert(None, int(item))
-        flag = False
-    else:
-        insert(root, int(item))
-inorder(root)
-print()
-preorder(root)
-print()
-postorder(root)
+nums1 = []
+nums2 = []
+for item in input().split(', '):
+  nums1.append(int(item))
+for item in input().split(', '):
+  nums2.append(int(item))
+m = int(input())
+n = int(input())
+#merge(nums1, m, nums2, n)
+print(merge(nums1, m, nums2, n))
